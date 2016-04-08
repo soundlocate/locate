5541abd5b0fa028ea95cffeb416047563f0fa245
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-04-08 15:47:51 +0200

    use bugs to make it work

--- src/PositionClient.cpp
+++ src/PositionClient.cpp
@@ -13,18 +13,11 @@ PositionClient::PositionClient(const char * ip, unsigned short port) {
 }
 
 int PositionClient::sendPositions(std::vector<v3> positions) {
-	u32	size = positions.size();
+	u32	size = positions.size() * 2;
 
-	if(size != 1) {
-		server->send(&size, sizeof(u32));
-		server->send(positions.data(), sizeof(v3) * size);
-	} else if(size == 1) {
-		size = 2;
-
-		server->send(&size, sizeof(u32));
-		server->send(positions.data(), sizeof(v3));
-		server->send(positions.data(), sizeof(v3));
-	}
+	server->send(&size, sizeof(u32));
+	server->send(positions.data(), sizeof(v3) * size);
+	server->send(positions.data(), sizeof(v3) * size);
 
 	return 0;
 }
