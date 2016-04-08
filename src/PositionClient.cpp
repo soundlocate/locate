8141e550a58a8b0ee0cc2fb9d4b23816a9ccb00d
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-04-08 17:56:53 +0200

    Revert "use bugs to make it work"
    This reverts commit 5541abd5b0fa028ea95cffeb416047563f0fa245.

--- src/PositionClient.cpp
+++ src/PositionClient.cpp
@@ -13,11 +13,18 @@ PositionClient::PositionClient(const char * ip, unsigned short port) {
 }
 
 int PositionClient::sendPositions(std::vector<v3> positions) {
-	u32	size = positions.size() * 2;
+	u32	size = positions.size();
 
-	server->send(&size, sizeof(u32));
-	server->send(positions.data(), sizeof(v3) * size);
-	server->send(positions.data(), sizeof(v3) * size);
+	if(size != 1) {
+		server->send(&size, sizeof(u32));
+		server->send(positions.data(), sizeof(v3) * size);
+	} else if(size == 1) {
+		size = 2;
+
+		server->send(&size, sizeof(u32));
+		server->send(positions.data(), sizeof(v3));
+		server->send(positions.data(), sizeof(v3));
+	}
 
 	return 0;
 }
